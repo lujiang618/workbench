bc784292e46a89f3e1ca9a39da615c2ee1c71d37
鲁江 <531432594@qq.com>
2020-12-11 23:11:51 +0800

    更新前端框架

--- web/src/main.js
+++ web/src/main.js
@@ -1,20 +1,42 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+// with polyfills
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
-// 引入vue-beauty
-import 'vue-beauty/package/style/vue-beauty.min.css'
-import vueBeauty from 'vue-beauty'
-Vue.use(vueBeauty)
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/'
+import i18n from './locales'
+import { VueAxios } from './utils/request'
+import ProLayout, { PageHeaderWrapper } from '@ant-design-vue/pro-layout'
+import themePluginConfig from '../config/themePluginConfig'
+
+// mock
+// WARNING: `mockjs` NOT SUPPORT `IE` PLEASE DO NOT USE IN `production` ENV.
+import './mock'
+
+import bootstrap from './core/bootstrap'
+import './core/lazy_use' // use lazy load components
+import './permission' // permission control
+import './utils/filter' // global filter
+import './global.less' // global style
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// mount axios to `Vue.$http` and `this.$http`
+Vue.use(VueAxios)
+// use pro-layout components
+Vue.component('pro-layout', ProLayout)
+Vue.component('page-container', PageHeaderWrapper)
+Vue.component('page-header-wrapper', PageHeaderWrapper)
+
+window.umi_plugin_ant_themeVar = themePluginConfig.theme
+
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  i18n,
+  // init localstorage, vuex
+  created: bootstrap,
+  render: h => h(App)
+}).$mount('#app')
